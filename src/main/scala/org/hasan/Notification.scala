31677321d7a08eca264f305d4177c3fe737b527b
Hasan Zaidi <hasan.zaidi@rocketmail.com>
2020-10-12 20:49:49 +0100

    Make case classes final.

--- src/main/scala/org/hasan/Notification.scala
+++ src/main/scala/org/hasan/Notification.scala
@@ -2,10 +2,8 @@ package org.hasan
 
 abstract class Notification
 
-case class Email(sender: String, title: String, body: String)
-    extends Notification
+final case class Email(sender: String, title: String, body: String) extends Notification
 
-case class SMS(caller: String, message: String) extends Notification
+final case class SMS(caller: String, message: String) extends Notification
 
-case class VoiceRecording(contactName: String, link: String)
-    extends Notification
+final case class VoiceRecording(contactName: String, link: String) extends Notification
--- src/main/scala/org/hasan/caseclass/Account.scala
+++ src/main/scala/org/hasan/caseclass/Account.scala
@@ -1,5 +1,4 @@
 package org.hasan.caseclass
 
 // Need an "override val" if extending a trait
-case class Account(override val policyNumber: String, inForce: Boolean)
-    extends AccountTrait
+final case class Account(override val policyNumber: String, inForce: Boolean) extends AccountTrait
